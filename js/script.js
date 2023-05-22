48760aed7cb2641b0009091bde99aa6b48d6da52
Adrian Wozniak <adrianwozniak20@gmail.com>
2023-05-22 21:25:44 +0200

    functions to js code added

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
-console.log("Hello, fellas developas!");
+{
+    const welcome = () => {
+        console.log("Hello, fellas developas!");
+    }
 
-let button1 = document.querySelector(".js-button1");
-let body = document.querySelector(".body");
-let button2 = document.querySelector(".js-button2");
-let header = document.querySelector(".body__header");
+    const toggleBackground = (button1) => {
+        const body = document.querySelector(".body");
+        body.classList.toggle("body--dark");
+        button1.innerText = body.classList.contains("body--dark") ? "Jasny kolor tła" : "Ciemny kolor tła";
+    }
 
-button1.addEventListener("click", () => {
-    body.classList.toggle("body--dark");
-    
-    button1.innerText = body.classList.contains("body--dark")
-    ?
-    "Jasny kolor tła"
-    :
-    "Ciemny kolor tła";
-})
+    const toggleHeaderColor = () => {
+        const header = document.querySelector(".body__header");
+        header.classList.toggle("body__header--changed");
+    }
 
-button2.addEventListener("click", () => {
-    header.classList.toggle("body__header--changed");
-})
+    const init = () => {
+        const button1 = document.querySelector(".js-button1");
+        const button2 = document.querySelector(".js-button2");
+
+        button1.addEventListener("click", toggleBackground);
+        button2.addEventListener("click", toggleHeaderColor);
+
+        welcome();
+    }
+
+    init();
+}
